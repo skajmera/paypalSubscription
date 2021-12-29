735ffadbec4375f01c5c407fe7f4970644e4bce8
skajmera <subhashajmera2@gmail.com>
2021-12-29 14:47:09 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,88 @@
+const express = require('express');
+const paypal = require('paypal-rest-sdk');
+require('dotenv').config()
+const PORT=process.env.PORT
+paypal.configure({
+  'mode': 'sandbox', //sandbox or live
+  'client_id': process.env.client_id,
+  'client_secret': process.env.client_secret
+});
+
+const app = express();
+
+app.get('/', (req, res) => res.sendFile(__dirname + "/index.html"));
+
+app.get('/pay', (req, res) => {
+    const create_payment_json = {
+      "intent": "sale",
+      "payer": {
+          "payment_method": "paypal"
+      },
+      "redirect_urls": {
+          "return_url": "http://localhost:3000/success",
+          "cancel_url": "http://localhost:3000/cancel"
+      },
+      "transactions": [{
+          "item_list": {
+              "items": [{
+                  "name": "Redhock Bar Soap",
+                  "sku": "001",
+                  "price": "25.00",
+                  "currency": "USD",
+                  "quantity": 1
+              }]
+          },
+          "amount": {
+              "currency": "USD",
+              "total": "25.00"
+          },
+          "description": "Washing Bar soap"
+      }]
+  };
+  
+  paypal.payment.create(create_payment_json, function (error, payment) {
+    if (error) {
+        throw error;
+    } else {
+        for(let i = 0;i < payment.links.length;i++){
+          if(payment.links[i].rel === 'approval_url'){
+            res.redirect(payment.links[i].href);
+          }
+        }
+    }
+  });
+  });
+
+
+  app.get('/success', (req, res) => {
+    const payerId = req.query.PayerID;
+    const paymentId = req.query.paymentId;
+  
+    const execute_payment_json = {
+      "payer_id": payerId,
+      "transactions": [{
+          "amount": {
+              "currency": "USD",
+              "total": "25.00"
+          }
+      }]
+    };
+  
+  // Obtains the transaction details from paypal
+    paypal.payment.execute(paymentId, execute_payment_json, function (error, payment) {
+        //When error occurs when due to non-existent transaction, throw an error else log the transaction details in the console then send a Success string reposponse to the user.
+      if (error) {
+          console.log(error.response);
+          throw error;
+      } else {
+          console.log(JSON.stringify(payment));
+          res.send('Success');
+      }
+  });
+  });
+
+  app.get('/cancel', (req, res) => res.send('Cancelled'));
+
+app.listen(PORT, () => console.log(`Server Started on ${PORT}`));
+
+////// sb-igycp10786845@personal.example.com
